93e4d942909c0002aaf0541709ab13f2efac0b69
john <xjohnishere19@aol.com>
2017-07-26 23:01:20 -0400

    Update hibernate config to read from properties file and refactor

--- src/main/java/com/revature/util/HibernateUtil.java
+++ src/main/java/com/revature/util/HibernateUtil.java
@@ -7,28 +7,34 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
-public class HibernateUtil {
-	private static SessionFactory sf = null;
-	private static SessionFactory sessionFactory(String filename) {
-		if(sf == null){
-		Configuration c = new Configuration().configure(filename);
-		ServiceRegistry sr = new StandardServiceRegistryBuilder().applySettings(
-				c.getProperties()).build();
-			sf =  c.buildSessionFactory(sr);
-		return sf;
-		} else {
-			return sf;
-		} 
-	}
+import java.io.IOException;
+import java.util.Properties;
 
-	public static Session getSession() {
-		return sessionFactory("Hibernate.cfg.xml").openSession();
-	}
-	
-	public static void closeFactory(){
-		if(sf != null){
-			sf.close();
-			sf = null;
-		}
-	}
+public class HibernateUtil
+{
+    private static final SessionFactory sf = sessionFactory();
+
+    private static SessionFactory sessionFactory()
+    {
+        Properties prop = new Properties();
+
+        try
+        {
+            prop.load(HibernateUtil.class.getClassLoader().getResourceAsStream("hibernate.properties"));
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        Configuration c = new Configuration().configure("Hibernate.cfg.xml");
+        c.mergeProperties(prop);
+        ServiceRegistry sr = new StandardServiceRegistryBuilder().applySettings(c.getProperties()).build();
+
+        return c.buildSessionFactory(sr);
+    }
+
+    public static Session getSession()
+    {
+        return sf.openSession();
+    }
 }
